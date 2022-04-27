1c5b179125c1f85d958434c7369e5c44effefe36
Moses Chawawa <moseschawawa@gmail.com>
2022-04-27 14:55:32 +0200

    added appointment statistics

--- potlako_reports/views/sync_report.py
+++ potlako_reports/views/sync_report.py
@@ -12,6 +12,7 @@ class SyncReportView(NavbarViewMixin, EdcBaseViewMixin, TemplateView):
     navbar_name = 'potlako_reports'
 
     subject_consent_model = 'potlako_subject.subjectconsent'
+    appointment_model = 'edc_appointment.appointment'
 
     packages = ('edc_appointment', 'potlako_subject'),
 
@@ -21,13 +22,42 @@ class SyncReportView(NavbarViewMixin, EdcBaseViewMixin, TemplateView):
     def subject_consent_cls(self):
         return django_apps.get_model(self.subject_consent_model)
 
+    @property
+    def appointment_cls(self):
+        return django_apps.get_model(self.appointment_model)
 
-    def calculate_statistics(self):
-
-        statistics = dict()
-
+    @property
+    def host_machines(self):
         host_machines = set(self.subject_consent_cls.objects.values_list(
             'hostname_created', flat=True))
+        return host_machines
+
+    @property
+    def appointment_statistics(self):
+
+        statistics = []
+
+        for host_machine in self.host_machines:
+
+            appointments = self.appointment_cls.objects.filter(
+                hostname_created=host_machine)
+
+            statistics.append(
+                {
+                    'host_machine': host_machine,
+                    'appointments': appointments.count(),
+                    'last_modified': appointments.latest('modified')    
+                }
+            )
+        
+        return statistics
+            
+
+
+    @property
+    def general_statistics(self):
+
+        statistics = dict()
 
         models = {}
 
@@ -37,7 +67,7 @@ class SyncReportView(NavbarViewMixin, EdcBaseViewMixin, TemplateView):
             if temp:
                 models.update(temp)
 
-        for host_machine in host_machines:
+        for host_machine in self.host_machines:
 
             host_machine_statistics = dict()
 
@@ -60,9 +90,10 @@ class SyncReportView(NavbarViewMixin, EdcBaseViewMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        statistics = self.calculate_statistics()
-
-        context.update(statistics=statistics)
+        context.update(
+            host_machines = self.host_machines,
+            general_statistics=self.general_statistics,
+            appointment_statistics = self.appointment_statistics)
         return context
     
 
