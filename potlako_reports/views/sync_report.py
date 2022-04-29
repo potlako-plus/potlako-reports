44169c502c1da5bd67b6d9e15153ea37ba7f8e43
Moses Chawawa <moseschawawa@gmail.com>
2022-04-29 12:49:34 +0200

    crf sync report

--- potlako_reports/views/sync_report.py
+++ potlako_reports/views/sync_report.py
@@ -1,8 +1,11 @@
+from itertools import count
+import statistics
 from django.apps import apps as django_apps
 from django.views.generic import TemplateView
 
 from edc_base.view_mixins import EdcBaseViewMixin
 from edc_navbar import NavbarViewMixin
+
 
 
 
@@ -14,7 +17,7 @@ class SyncReportView(NavbarViewMixin, EdcBaseViewMixin, TemplateView):
     subject_consent_model = 'potlako_subject.subjectconsent'
     appointment_model = 'edc_appointment.appointment'
 
-    packages = ('edc_appointment', 'potlako_subject'),
+    modules = ('edc_appointment', 'potlako_subject'),
 
     
 
@@ -33,6 +36,37 @@ class SyncReportView(NavbarViewMixin, EdcBaseViewMixin, TemplateView):
         return host_machines
 
     @property
+    def host_machines_formatted_names(self):
+        host_machines = []
+        for name in self.host_machines:
+            if 'potlako_plus' in name:
+                host_machines.append(name[-2:])
+        return host_machines
+
+    @property
+    def crf_models(self):
+
+        models = list()
+
+        for module in self.modules:
+            for model in django_apps.get_models():
+                if not model.__module__.startswith(module) \
+                        or model._meta.verbose_name.startswith('historical'):
+                    continue
+                models.append(model)
+
+        return models
+
+    @property
+    def crf_verbose_names(self):
+        verbose_names = []
+
+        for model_cls in self.crf_models:
+            verbose_names.append(model_cls._meta.verbose_name.title())
+        return verbose_names
+
+
+    @property
     def appointment_statistics(self):
 
         statistics = []
@@ -46,11 +80,28 @@ class SyncReportView(NavbarViewMixin, EdcBaseViewMixin, TemplateView):
                 {
                     'host_machine': host_machine,
                     'appointments': appointments.count(),
-                    'last_modified': appointments.latest('modified')    
+                    'last_modified': appointments.latest('modified').modified.date().isoformat()    
                 }
             )
         
         return statistics
+
+    @property
+    def crf_statistics(self):
+        statistics = []
+        
+        for crf in self.crf_models:
+            data = [crf._meta.verbose_name,]
+            for host_machine in self.host_machines:
+                crf_counter = crf.objects.filter(hostname_created=host_machine).count()
+                data.append(crf_counter)
+            statistics.append(data)
+                
+
+                
+        return statistics
+
+        
             
 
 
@@ -61,7 +112,7 @@ class SyncReportView(NavbarViewMixin, EdcBaseViewMixin, TemplateView):
 
         models = {}
 
-        for package in self.packages:
+        for package in self.modules:
             temp = django_apps.all_models.get(package, None)
 
             if temp:
@@ -91,9 +142,13 @@ class SyncReportView(NavbarViewMixin, EdcBaseViewMixin, TemplateView):
         context = super().get_context_data(**kwargs)
 
         context.update(
+            crf_verbose_names=self.crf_verbose_names,
+            crf_statistics = self.crf_statistics,
             host_machines = self.host_machines,
+            host_machines_formatted_names=self.host_machines_formatted_names,
             general_statistics=self.general_statistics,
-            appointment_statistics = self.appointment_statistics)
+            appointment_statistics = self.appointment_statistics,
+            crf_models=self.crf_models)
         return context
     
 
