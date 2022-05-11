e64b11d21adec8ca22b7164adafc4928e4bb4843
Moses Chawawa <moseschawawa@gmail.com>
2022-05-11 12:24:36 +0200

    Added non Crf statistics

--- potlako_reports/views/sync_report.py
+++ potlako_reports/views/sync_report.py
@@ -1,5 +1,9 @@
+import enum
 from itertools import count
 import statistics
+from tabnanny import verbose
+from textwrap import indent
+from xml.sax.handler import property_encoding
 from django.apps import apps as django_apps
 from django.views.generic import TemplateView
 
@@ -9,147 +13,113 @@ from edc_navbar import NavbarViewMixin
 
 
 
-class SyncReportView(NavbarViewMixin, EdcBaseViewMixin, TemplateView):
+class SyncReportView(TemplateView, NavbarViewMixin, EdcBaseViewMixin):
     template_name = 'sync_report.html'
     navbar_selected_item = 'Sync Report'
     navbar_name = 'potlako_reports'
 
-    subject_consent_model = 'potlako_subject.subjectconsent'
+    # Non crf models of interest
     appointment_model = 'edc_appointment.appointment'
-
-    modules = ('edc_appointment', 'potlako_subject'),
-
+    death_report_model = 'potlako_prn.deathreport'
+    subject_offstudy_model = 'potlako_prn.subjectoffstudy'
+    subject_consent_model = 'potlako_subject.subjectconsent'
+    subject_locator_model = 'potlako_subject.subjectlocator'
+    subject_screening_model = 'potlako_subject.subjectscreening'
+    subject_visit_model = 'potlako_subject.subjectvisit'
+    verbal_consent_model = 'potlako_subject.verbalconsent'
     
-
     @property
-    def subject_consent_cls(self):
-        return django_apps.get_model(self.subject_consent_model)
-
-    @property
-    def appointment_cls(self):
+    def appointment_model_cls(self):
         return django_apps.get_model(self.appointment_model)
+    
+    
+    @property
+    def verbal_consent_model_cls(self):
+        return django_apps.get_model(self.verbal_consent_model)
+    
+    @property
+    def death_report_model_cls(self):
+        return django_apps.get_model(self.death_report_model)
+    
+    @property
+    def subject_offstudy_model_cls(self):
+        return django_apps.get_model(self.subject_offstudy_model)
+
+    @property
+    def subject_consent_model_cls(self):
+        return django_apps.get_model(self.subject_consent_model)
+    
+    @property
+    def subject_locator_model_cls(self):
+        return django_apps.get_model(self.subject_locator_model)
+    
+    @property
+    def subject_screening_model_cls(self):
+        return django_apps.get_model(self.subject_screening_model)
+    
+    @property
+    def subject_visit_model_cls(self):
+        return django_apps.get_model(self.subject_visit_model)
 
     @property
     def host_machines(self):
-        host_machines = set(self.subject_consent_cls.objects.values_list(
-            'hostname_created', flat=True))
-        return host_machines
+        name_pattern = '[a-z_]+[0-9]+'
+        host_names  = set(self.subject_consent_model_cls.objects.filter(
+            hostname_created__iregex = name_pattern
+        ).values_list('hostname_created', flat=True))
 
-    @property
-    def host_machines_formatted_names(self):
-        host_machines = []
-        for name in self.host_machines:
-            if 'potlako_plus' in name:
-                host_machines.append(name[-2:])
-        return host_machines
-
-    @property
-    def crf_models(self):
-
-        models = list()
-
-        for module in self.modules:
-            for model in django_apps.get_models():
-                if not model.__module__.startswith(module) \
-                        or model._meta.verbose_name.startswith('historical'):
-                    continue
-                models.append(model)
-
-        return models
-
-    @property
-    def crf_verbose_names(self):
-        verbose_names = []
-
-        for model_cls in self.crf_models:
-            verbose_names.append(model_cls._meta.verbose_name.title())
-        return verbose_names
-
-
-    @property
-    def appointment_statistics(self):
-
-        statistics = []
-
-        for host_machine in self.host_machines:
-
-            appointments = self.appointment_cls.objects.filter(
-                hostname_created=host_machine)
-
-            statistics.append(
-                {
-                    'host_machine': host_machine,
-                    'appointments': appointments.count(),
-                    'last_modified': appointments.latest('modified').modified.date().isoformat()    
-                }
-            )
         
-        return statistics
+
+        return sorted(host_names)
+    
+    @property
+    def host_formatted_names(self):
+        return list(map(lambda name: f'Machine {name[-2:]}', self.host_machines))
 
     @property
-    def crf_statistics(self):
+    def non_crf_statistics(self):
         statistics = []
+        models_classes = [
+            self.subject_visit_model_cls, 
+            self.subject_screening_model_cls, 
+            self.subject_consent_model_cls,
+            self.verbal_consent_model_cls,
+            self.appointment_model_cls,
+            self.subject_offstudy_model_cls,
+            self.death_report_model_cls,
+            self.subject_locator_model_cls,
+        ]
         
-        for crf in self.crf_models:
-            data = [crf._meta.verbose_name,]
+        
+        for model_class in models_classes:
+            model_class_statistics = []
+            
+            verbose_name = model_class._meta.verbose_name.title()
+            
+            model_class_statistics.append(verbose_name)
+                
             for host_machine in self.host_machines:
-                crf_counter = crf.objects.filter(hostname_created=host_machine).count()
-                data.append(crf_counter)
-            statistics.append(data)
+                counter = model_class.objects.filter(hostname_created__iexact=host_machine).count()
+                model_class_statistics.append(counter)
                 
-
-                
-        return statistics
-
+            statistics.append(model_class_statistics)
         
             
-
-
-    @property
-    def general_statistics(self):
-
-        statistics = dict()
-
-        models = {}
-
-        for package in self.modules:
-            temp = django_apps.all_models.get(package, None)
-
-            if temp:
-                models.update(temp)
-
-        for host_machine in self.host_machines:
-
-            host_machine_statistics = dict()
-
-            for model_cls in models.values():
-
-                if 'historical' in model_cls._meta.verbose_name:
-                    continue
-                try:
-                    count = model_cls.objects.filter(
-                        hostname_created=host_machine).count()
-                    host_machine_statistics.update(
-                        {model_cls._meta.verbose_name: count})
-                except:
-                    continue
-
-            statistics.update({host_machine: host_machine_statistics})
-
         return statistics
-
+    
+    
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
+        context =  super().get_context_data(**kwargs)
+        
         context.update(
-            crf_verbose_names=self.crf_verbose_names,
-            crf_statistics = self.crf_statistics,
-            host_machines = self.host_machines,
-            host_machines_formatted_names=self.host_machines_formatted_names,
-            general_statistics=self.general_statistics,
-            appointment_statistics = self.appointment_statistics,
-            crf_models=self.crf_models)
+            non_crf_statistics=self.non_crf_statistics,
+            host_machines = self.host_formatted_names
+        )
+        
         return context
+
+
+    
     
 
 
